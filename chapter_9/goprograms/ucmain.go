ee41063341c6d2b08a76f608d49c845d84a62c4e
endingman <631925002@qq.com>
2019-12-20 17:19:14 +0800

    chapter_9

---
+++ chapter_9/goprograms/ucmain.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"./uc/uc"
+)
+
+func main() {
+	str1 := "USING package uc!"
+	fmt.Println(uc.UpperCase(str1))
+}
---
+++ chapter_9/package_sample_custom.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"go_module/chapter_9/pack1" //module模式引入自定义包 模块名称/路径.../包名
+)
+
+func main() {
+	var test1 string
+	test1 = pack1.ReturnStr()
+	fmt.Printf("ReturnStr from package1: %s\n", test1)
+	fmt.Printf("Integer from package1: %d\n", pack1.Pack1Int)
+	// fmt.Printf("Float from package1: %f\n", pack1.pack1Float)
+}
