7755e1e63a5b89d35881cb1630f0948a0ed5165e
endingman <631925002@qq.com>
2019-12-27 11:06:23 +0800

    chapter_15

--- chapter_15/webserver/twitter_status.go
+++ chapter_15/webserver/twitter_status.go
@@ -23,6 +23,7 @@ func main() {
 	// 初始化XML返回值的结构
 	user := User{xml.Name{"", "user"}, Status{""}}
 	// 将XML解析为我们的结构
+	// 你可能无法获取到 xml 数据，另外由于 go 版本的更新，xml.Unmarshal 函数的第一个参数需是 [] byte 类型，而无法传入 Body。
 	xml.Unmarshal(response.Body, &user)
 	fmt.Printf("status: %s", user.Status.Text)
 }
