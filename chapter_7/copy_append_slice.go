66d2c1a63938772cda1e46746f0665b99f731990
endingman <631925002@qq.com>
2019-12-28 17:20:54 +0800

    修改append一点注释

--- chapter_7/copy_append_slice.go
+++ chapter_7/copy_append_slice.go
@@ -7,27 +7,27 @@ import "fmt"
 下面的代码描述了从拷贝切片的 copy 函数和向切片追加新元素的 append 函数。
 
 append 非常有用，它能够用于各种方面的操作
-将切片 b 的元素追加到切片 a 之后：a = append(a, b...)
+1、将切片 b 的元素追加到切片 a 之后：a = append(a, b...)
 
-复制切片 a 的元素到新的切片 b 上：
+2、复制切片 a 的元素到新的切片 b 上：
 
 b = make([]T, len(a))
 copy(b, a)
-删除位于索引 i 的元素：a = append(a[:i], a[i+1:]...)
+3、删除位于索引 i 的元素：a = append(a[:i], a[i+1:]...)
 
-切除切片 a 中从索引 i 至 j 位置的元素：a = append(a[:i], a[j:]...)
+4、切除切片 a 中从索引 i 至 j 位置的元素：a = append(a[:i], a[j:]...)
 
-为切片 a 扩展 j 个元素长度：a = append(a, make([]T, j)...)
+5、为切片 a 扩展 j 个元素长度：a = append(a, make([]T, j)...)
 
-在索引 i 的位置插入元素 x：a = append(a[:i], append([]T{x}, a[i:]...)...)
+6、在索引 i 的位置插入元素 x：a = append(a[:i], append([]T{x}, a[i:]...)...)
 
-在索引 i 的位置插入长度为 j 的新切片：a = append(a[:i], append(make([]T, j), a[i:]...)...)
+7、在索引 i 的位置插入长度为 j 的新切片：a = append(a[:i], append(make([]T, j), a[i:]...)...)
 
-在索引 i 的位置插入切片 b 的所有元素：a = append(a[:i], append(b, a[i:]...)...)
+8、在索引 i 的位置插入切片 b 的所有元素：a = append(a[:i], append(b, a[i:]...)...)
 
-取出位于切片 a 最末尾的元素 x：x, a = a[len(a)-1], a[:len(a)-1]
+9、取出位于切片 a 最末尾的元素 x：x, a = a[len(a)-1], a[:len(a)-1]
 
-将元素 x 追加到切片 a：a = append(a, x)
+10、将元素 x 追加到切片 a：a = append(a, x)
 * @Author Liumm
 * @Date   2019-12-20
 * @return {[type]}   [description]
