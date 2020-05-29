e468768a2ebca4fa127686f8b9beb8496bdd1098
endingman <631925002@qq.com>
2020-05-29 14:14:13 +0800

    chapter_5增加一些内容

--- chapter_5/for1.go
+++ chapter_5/for1.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-
+	// for基本形式 -> for 初始化语句; 条件语句; 修饰语句 {}
 	for i := 0; i < 5; i++ {
 		fmt.Printf("This is the %d iteration\n", i)
 	}
@@ -32,7 +32,10 @@ func main() {
 	//     fmt.Println("Value of s:", s)
 	//     s = s + "a" //
 	// }
+	// 您还可以在循环中同时使用多个计数器：
 
+	// for i, j := 0, N; i < j; i, j = i+1, j-1 {}
+	// 这得益于 Go 语言具有的平行赋值的特性
 	for i, j, s := 0, 5, "a"; i < 3 && j < 100 && s != "aaaaa"; i, j,
 		s = i+1, j+1, s+"a" { //for变量在for的block里生效
 		fmt.Println("Value of i, j, s:", i, j, s)
--- chapter_5/ifelse.go
+++ chapter_5/ifelse.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+/**
+使用简短方式 := 声明的变量的作用域只存在于 if 结构中（在 if 结构的大括号之间，如果使用 if-else 结构则在 else 代码块中变量也会存在）。如果变量在 if 结构之前就已经存在，那么在 if 结构中，该变量原来的值会被隐藏。最简单的解决方案就是不要在初始化语句中声明变量
+* @Author Liumm
+* @Date   2020-05-29
+* @return {[type]}   [description]
+*/
 func main() {
 	var first int = 10
 	var cond int
