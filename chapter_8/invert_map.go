e5c7a2f45d3405a004c1c05d4d0262e46cd0611d
endingman <631925002@qq.com>
2020-06-28 14:25:34 +0800

    map增加一些注释点

--- chapter_8/invert_map.go
+++ chapter_8/invert_map.go
@@ -11,6 +11,13 @@ var (
 		"juliet": 65, "kili": 43, "lima": 98}
 )
 
+/**
+ * key-value互换
+ * 如果原始 value 值不唯一那么这么做肯定会出错；为了保证不出错，当遇到不唯一的 key 时应当立刻停止，这样可能会导致没有包含原 map 的所有键值对！一种解决方法就是仔细检查唯一性并且使用多值 map，比如使用 map[int][]string 类型。
+ * @Author Liumm
+ * @Date   2020-06-28
+ * @return {[type]}   [description]
+ */
 func main() {
 	invMap := make(map[int]string, len(barVal))
 	for k, v := range barVal {
--- chapter_8/sort_map.go
+++ chapter_8/sort_map.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-    "fmt"
-    "sort"
+	"fmt"
+	"sort"
 )
 
 var (
-    barVal = map[string]int{"alpha": 34, "bravo": 56, "charlie": 23,
-        "delta": 87, "echo": 56, "foxtrot": 12,
-        "golf": 34, "hotel": 16, "indio": 87,
-        "juliet": 65, "kili": 43, "lima": 98}
+	barVal = map[string]int{"alpha": 34, "bravo": 56, "charlie": 23,
+		"delta": 87, "echo": 56, "foxtrot": 12,
+		"golf": 34, "hotel": 16, "indio": 87,
+		"juliet": 65, "kili": 43, "lima": 98}
 )
 
 /**
- * map都是无序的，排序
+ * map都是无序的，排序，不管是按照 key 还是按照 value 默认都不排序
  * 但是如果你想要一个排序的列表你最好使用结构体切片，这样会更有效
  * @Author Liumm
  * @Date   2019-08-20
  * @return {[type]}   [description]
  */
 func main() {
-    fmt.Println("unsorted:")
-    for k, v := range barVal {
-        fmt.Printf("Key: %v, Value: %v / ", k, v)
-    }
-    keys := make([]string, len(barVal))
-    i := 0
-    for k, _ := range barVal {
-        keys[i] = k
-        i++
-    }
-    sort.Strings(keys)
-    fmt.Println()
-    fmt.Println("sorted:")
-    for _, k := range keys {
-        fmt.Printf("Key: %v, Value: %v / ", k, barVal[k])
-    }
+	fmt.Println("unsorted:")
+	for k, v := range barVal {
+		fmt.Printf("Key: %v, Value: %v / ", k, v)
+	}
+	keys := make([]string, len(barVal))
+	i := 0
+	for k := range barVal {
+		keys[i] = k
+		i++
+	}
+	sort.Strings(keys)
+	fmt.Println()
+	fmt.Println("sorted:")
+	for _, k := range keys {
+		fmt.Printf("Key: %v, Value: %v / ", k, barVal[k])
+	}
 }
